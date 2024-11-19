914b8c180bb28b0e0ae08a08dfee152e6ab86426
FBI1337 <FBI13377@yandex.ru>
2024-11-19 16:54:45 +0300

    no use

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,14 @@
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GenderColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BirhdayColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EnducationColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Score_Math = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Score_rus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Score_IT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total_Score = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.toolStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
@@ -44,18 +52,30 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 28);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameColumn,
+            this.GenderColumn,
+            this.BirhdayColumn,
+            this.EnducationColumn,
+            this.Score_Math,
+            this.Score_rus,
+            this.Score_IT,
+            this.Total_Score});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 34);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(794, 388);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1059, 478);
             this.dataGridView1.TabIndex = 0;
             // 
             // toolStrip1
             // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(794, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(1059, 27);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -65,48 +85,107 @@
             this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 24);
             this.toolStripButton1.Text = "toolStripButton1";
             this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
             // 
             // statusStrip1
             // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this.toolStripStatusLabel2,
             this.toolStripStatusLabel3});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 419);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 517);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(794, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1059, 26);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
             this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
             // 
             // toolStripStatusLabel2
             // 
             this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
-            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(151, 20);
             this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
             // 
             // toolStripStatusLabel3
             // 
             this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
-            this.toolStripStatusLabel3.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(151, 20);
             this.toolStripStatusLabel3.Text = "toolStripStatusLabel3";
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.HeaderText = "ФИО";
+            this.NameColumn.MinimumWidth = 6;
+            this.NameColumn.Name = "NameColumn";
+            this.NameColumn.Width = 125;
+            // 
+            // GenderColumn
+            // 
+            this.GenderColumn.HeaderText = "Пол";
+            this.GenderColumn.MinimumWidth = 6;
+            this.GenderColumn.Name = "GenderColumn";
+            this.GenderColumn.Width = 125;
+            // 
+            // BirhdayColumn
+            // 
+            this.BirhdayColumn.HeaderText = "Дата рождения";
+            this.BirhdayColumn.MinimumWidth = 6;
+            this.BirhdayColumn.Name = "BirhdayColumn";
+            this.BirhdayColumn.Width = 125;
+            // 
+            // EnducationColumn
+            // 
+            this.EnducationColumn.HeaderText = "Форма обучения";
+            this.EnducationColumn.MinimumWidth = 6;
+            this.EnducationColumn.Name = "EnducationColumn";
+            this.EnducationColumn.Width = 125;
+            // 
+            // Score_Math
+            // 
+            this.Score_Math.HeaderText = "Баллы по математике";
+            this.Score_Math.MinimumWidth = 6;
+            this.Score_Math.Name = "Score_Math";
+            this.Score_Math.Width = 125;
+            // 
+            // Score_rus
+            // 
+            this.Score_rus.HeaderText = "Баллы по русскому";
+            this.Score_rus.MinimumWidth = 6;
+            this.Score_rus.Name = "Score_rus";
+            this.Score_rus.Width = 125;
+            // 
+            // Score_IT
+            // 
+            this.Score_IT.HeaderText = "Баллы по информатике";
+            this.Score_IT.MinimumWidth = 6;
+            this.Score_IT.Name = "Score_IT";
+            this.Score_IT.Width = 125;
+            // 
+            // Total_Score
+            // 
+            this.Total_Score.HeaderText = "Общая сумма баллов";
+            this.Total_Score.MinimumWidth = 6;
+            this.Total_Score.Name = "Total_Score";
+            this.Total_Score.Width = 125;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(794, 441);
+            this.ClientSize = new System.Drawing.Size(1059, 543);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -128,6 +207,14 @@
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GenderColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BirhdayColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EnducationColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Score_Math;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Score_rus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Score_IT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total_Score;
     }
 }
 
