5f8e804a3f0f269e1c54784ca3a663ad3b5f22f7
FBI1337 <84200204+FBI1337@users.noreply.github.com>
2024-11-27 19:03:16 +0300

    unit Test

---
+++ DataGridViewShelkynov.PersonManager.Test/PersonManagerTest.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DataGridView_Shelkynov.Models;
+using DataGridViewShelkynov.Contracts;
+using DataGridViewShelkynov.Contracts.Models;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace DataGridViewShelkynov.PersonManager.Test
+{
+
+    /// <summary>
+    /// Тесты для класса <see cref="PersonManager"/>.
+    /// </summary>
+    public class PersonManagerTest
+    {
+        private readonly IPersonManager personManager;
+        private readonly Mock<IPersonStorage> personStorageMock;
+        private readonly Mock<ILogger> loggerMock;
+
+
+        /// <summary>
+        /// Инициализирует новый класс <see cref="PersonManagerTest"/>
+        /// </summary>
+        public PersonManagerTest()
+        {
+            personStorageMock = new Mock<IPersonStorage>();
+            loggerMock = new Mock<ILogger>();
+
+            loggerMock.Setup(x => x.Log(LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()));
+            personManager = new PersonManager(personStorageMock.Object, loggerMock.Object);
+        }
+
+
+        /// <summary>
+        /// Тесе: Метод <see cref="PersonManager.AddAsync"/>
+        /// </summary>
+        [Fact]
+        public async Task AddShouldWork()
+        {
+            //Arrange
+            var model = new Person
+            {
+                Id = Guid.NewGuid(),
+                Name = $"Name{Guid.NewGuid():N}",
+                Gender = Gender.Famele,
+                Birhday = DateTime.Now,
+                Education = Enducation.BEER,
+                Value1 = 30,
+                Value2 = 30,
+                Value3 = 30,
+            };
+            personStorageMock.Setup(x => x.AddAsync(It.IsAny<Person>()))
+                .ReturnsAsync(model);
+
+            // Act
+            var result = await personManager.AddAsync(model);
+
+            // Asset
+            result.Should().NotBeNull()
+                .And.Be(model);
+
+            loggerMock.Verify(x => x.Log
+            (LogLevel.Information,
+            It.IsAny<EventId>(),
+            It.Is<It.IsAnyType>((state, t) => state.ToString().Contains(nameof(IPersonManager.AddAsync))),
+            null,
+            It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+            Times.Once);
+            loggerMock.VerifyNoOtherCalls();
+
+            personStorageMock.Verify(x => x.AddAsync(It.Is<Person>(y => y.Id == model.Id)),
+                Times.Once);
+            personStorageMock.VerifyNoOtherCalls();
+        }
+
+        /// <summary>
+        /// Тест: Метод <see cref="PersonManager.EditAsync"/>
+        /// </summary>
+        [Fact]
+        public async Task EditShouldWork()
+        {
+            //Arrange
+            var model = new Person
+            {
+                Id = Guid.NewGuid(),
+                Name = $"Name{Guid.NewGuid():N}",
+                Gender = Gender.Famele,
+                Birhday = DateTime.Now,
+                Education = Enducation.BEER,
+                Value1 = 30,
+                Value2 = 30,
+                Value3 = 30,
+            };
+            personStorageMock.Setup(x => x.EditAsync(It.IsAny<Person>())).Returns(Task.CompletedTask);
+
+            // Act
+            await personManager.EditAsync(model);
+
+            // Asset
+            loggerMock.Verify(x => x.Log
+            (LogLevel.Information,
+            It.IsAny<EventId>(),
+            It.Is<It.IsAnyType>((state, t) => state.ToString().Contains(nameof(IPersonManager.EditAsync))),
+            null,
+            It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+            Times.Once);
+            loggerMock.VerifyNoOtherCalls();
+
+            personStorageMock.Verify(x => x.EditAsync(It.Is<Person>(y => y.Id == model.Id)),
+                Times.Once);
+            personStorageMock.VerifyNoOtherCalls();
+        }
+
+        /// <summary>
+        /// Тест: метод <see cref="PersonManager.DeleteAsync"/>
+        /// </summary>
+        [Fact]
+        public async Task DeleteShouldWork()
+        {
+            //Arrange
+            var model = new Person
+            {
+                Id = Guid.NewGuid(),
+                Name = $"Name{Guid.NewGuid():N}",
+                Gender = Gender.Famele,
+                Birhday = DateTime.Now,
+                Education = Enducation.BEER,
+                Value1 = 30,
+                Value2 = 30,
+                Value3 = 30
+            };
+            personStorageMock.Setup(x => x.DeleteAsync(model.Id))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await personManager.DeleteAsync(model.Id);
+
+            // Asset
+            result.Should().BeTrue();
+
+            loggerMock.Verify(x => x.Log
+            (LogLevel.Information,
+            It.IsAny<EventId>(),
+            It.Is<It.IsAnyType>((state, t) => state.ToString().Contains(nameof(IPersonManager.DeleteAsync))),
+            null,
+            It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+            Times.Once);
+            loggerMock.VerifyNoOtherCalls();
+
+            personStorageMock.Verify(x => x.DeleteAsync(model.Id),
+                Times.Once);
+            personStorageMock.VerifyNoOtherCalls();
+        }
+
+
+        [Fact]
+        public async Task GetAllShouldWork()
+        {
+            // Arrange
+            var person = new List<Person>
+    {
+        new Person
+        {
+            Id = Guid.NewGuid(),
+            Name = "Person1",
+            Gender = Gender.Male,
+            Birhday = DateTime.Now.AddYears(-20),
+            Education = Enducation.BEER,
+            Value1 = 30,
+            Value2 = 30,
+            Value3 = 30
+        },
+        new Person
+        {
+            Id = Guid.NewGuid(),
+            Name = "Person2",
+            Gender = Gender.Famele,
+            Birhday = DateTime.Now.AddYears(-22),
+            Education = Enducation.FullTime,
+            Value1 = 70,
+            Value2 = 70,
+            Value3 = 70
+        }
+    };
+            personStorageMock.Setup(x => x.GetAllAsync())
+                .ReturnsAsync(person);
+
+            // Act
+            var result = await personManager.GetAllAsync();
+
+            // Assert
+            result.Should().NotBeNull()
+                .And.HaveCount(person.Count)
+                .And.BeEquivalentTo(person);
+            personStorageMock.Verify(x => x.GetAllAsync(), Times.Once);
+            personStorageMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task GetStatsShouldWork()
+        {
+            // Arrange
+            var person = new List<Person>
+    {
+        new Person
+        {
+            Id = Guid.NewGuid(),
+            Name = "Person1",
+            Gender = Gender.Famele,
+            Birhday = DateTime.Now.AddYears(-20),
+            Education = Enducation.BEER,
+            Value1 = 30,
+            Value2 = 30,
+            Value3 = 30
+        },
+        new Person
+        {
+            Id = Guid.NewGuid(),
+            Name = "Person2",
+            Gender = Gender.Famele,
+            Birhday = DateTime.Now.AddYears(-22),
+            Education = Enducation.FullTime,
+            Value1 = 70,
+            Value2 = 70,
+            Value3 = 70
+        }
+    };
+            personStorageMock.Setup(x => x.GetAllAsync())
+                .ReturnsAsync(person);
+
+            // Act
+            var result = await personManager.GetStatsAsync();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Count.Should().Be(person.Count);
+            result.MaleCount.Should().Be(1);
+            result.FemaleCount.Should().Be(1);
+            result.FullTimeCount.Should().Be(1);
+            result.BEER.Should().Be(1);
+            result.FiftyFifty.Should().Be(0);
+            result.Result.Should().Be(2);
+
+            personStorageMock.Verify(x => x.GetAllAsync(), Times.Once);
+            personStorageMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ DtaGridViewShelkynov.StoreMemory.Test/PersonStoreMemoryTest.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataGridView_Shelkynov.Models;
+using DataGridViewShelkynov.Contracts;
+using DataGridViewShelkynov.Contracts.Models;
+using DtaGridViewShelkynov.StoreMemory;
+using DtaGridViewShelkynovStoreMemory;
+using FluentAssertions;
+using Xunit;
+
+namespace DtaGridViewShelkynov.StoreMemory.Test
+{
+    public class PersonStoreMemoryTest
+    {
+        /// <summary>
+        /// Тесты для класса <see cref="MemoryPersonStorage"/>
+        /// </summary>
+        public class PersonStoreMemoryTests
+        {
+            private readonly IPersonStorage personStorage;
+
+            /// <summary>
+            /// Инициализирует новый экземпляр класса <see cref="PersonStoreMemoryTests"/>
+            /// </summary>
+            public PersonStoreMemoryTests()
+            {
+                personStorage = new MemoryPersonStorage();
+            }
+
+            /// <summary>
+            /// При пустом списке нет ошибок <see cref="IPersonStorage.GetAllAsync"/>
+            /// </summary>
+            [Fact]
+            public async Task GetAllShouldNotThrow()
+            {
+                // Act
+                Func<Task> act = () => personStorage.GetAllAsync();
+
+                // Assert
+                await act.Should().NotThrowAsync();
+            }
+
+
+            /// <summary>
+            /// Получает пустой список <see cref="IPersonStorage.GetAllAsync"/>
+            /// </summary>
+            [Fact]
+            public async Task GetAllShouldReturnEmpty()
+            {
+                // Act
+                var result = await personStorage.GetAllAsync();
+
+                // Assert
+                result.Should()
+                .NotBeNull()
+                .And.BeEmpty();
+            }
+
+            /// <summary>
+            /// Тест: Метод <see cref="IPersonStorage.AddAsync"/> должен вернуть добавленный объект.
+            /// </summary>
+            [Fact]
+            public async Task AddShouldReturnValue()
+            {
+                // Arrange
+                var model = new Person
+                {
+                    Id = Guid.NewGuid(),
+                    Name = $"Name{Guid.NewGuid():N}",
+                    Gender = Gender.Famele,
+                    Birhday = DateTime.Now,
+                    Education = Enducation.BEER,
+                    Value1 = 30,
+                    Value2 = 30,
+                    Value3 = 30,
+                };
+
+                // Act
+                var result = await personStorage.AddAsync(model);
+
+                // Assert
+                result.Should().NotBeNull()
+                    .And.BeEquivalentTo(new
+                    {
+                        model.Id,
+                        model.Gender,
+                    });
+            }
+
+            /// <summary>
+            /// Удаление из хранилища
+            /// </summary>
+            [Fact]
+            public async Task DeleteShouldReturnTrue()
+            {
+                // Arrange
+                var model = new Person
+                {
+                    Id = Guid.NewGuid(),
+                    Name = $"Name{Guid.NewGuid():N}",
+                    Gender = Gender.Famele,
+                    Birhday = DateTime.Now,
+                    Education = Enducation.BEER,
+                    Value1 = 30,
+                    Value2 = 30,
+                    Value3 = 30,
+                };
+
+                // Act
+                await personStorage.AddAsync(model);
+                var result = await personStorage.DeleteAsync(model.Id);
+
+                var nailList = await personStorage.GetAllAsync();
+                var tryGetModel = nailList.FirstOrDefault(x => x.Id == model.Id);
+
+                // Assert
+                result.Should().BeTrue();
+                tryGetModel.Should().BeNull();
+            }
+
+            /// <summary>
+            /// Изменение данных в хранилище
+            /// </summary>
+            [Fact]
+            public async Task EditShouldUpdateStorageData()
+            {
+                // Arrange
+                var model = new Person
+                {
+                    Id = Guid.NewGuid(),
+                    Name = $"Name{Guid.NewGuid():N}",
+                    Gender = Gender.Famele,
+                    Birhday = DateTime.Now,
+                    Education = Enducation.BEER,
+                    Value1 = 30,
+                    Value2 = 30,
+                    Value3 = 30,
+                };
+
+                // Act
+                await personStorage.AddAsync(model);
+                await personStorage.EditAsync(model);
+                var applicantList = await personStorage.GetAllAsync();
+                var result = applicantList.FirstOrDefault(x => x.Id == model.Id);
+
+                // Assert
+                result?.Should().NotBeNull();
+                result?.Id.Should().Be(model.Id);
+                result?.Gender.Should().Be(Gender.Male);
+            }
+        }
+    }
+}
