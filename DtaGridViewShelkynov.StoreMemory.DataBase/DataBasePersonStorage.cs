935b4b6a26cd2b24da6e1df3bafcf8b688f17d7f
FBI1337 <84200204+FBI1337@users.noreply.github.com>
2024-11-27 20:29:36 +0300

    Сделал БД

---
+++ DtaGridViewShelkynov.StoreMemory.DataBase/DataBasePersonStorage.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DataGridViewShelkynov.Contracts;
+using System.Data.Entity;
+using DataGridViewShelkynov.Contracts.Models;
+using System.Linq;
+using System.Xml.Linq;
+using DataGridView_Shelkynov.Models;
+
+namespace DtaGridViewShelkynov.StoreMemory.DataBase
+{
+    /// <summary>
+    /// Этот класс реализует интерфейс IPersonStorage с использованием базы данных.
+    /// </summary>
+    public class DataBasePersonStorage : IPersonStorage
+    {
+
+        /// <summary>
+        /// Добавляет нового заявителя в базу данных.
+        /// </summary>
+        public async Task<Person> AddAsync(Person person)
+        {
+            using (var context = new DataGridContext())
+            {
+                var persons = new Person
+                {
+                    Id = person.Id,
+                    Name = person.Name,
+                    Birhday = person.Birhday,
+                    Education = person.Education,
+                    Value1 = person.Value1,
+                    Value2 = person.Value2,
+                    Value3 = person.Value3,
+                };
+                context.Persons.Add(person);
+                await context.SaveChangesAsync();
+            }
+
+            return person;
+        }
+
+        /// <summary>
+        /// Удаляет заявителя из базы данных по идентификатору.
+        /// </summary>
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            using (var context = new DataGridContext())
+            {
+                var item = await context.Persons.FirstOrDefaultAsync(x => x.Id == id);
+                if (item != null)
+                {
+                    context.Persons.Remove(item);
+                    await context.SaveChangesAsync();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Обновляет существующего заявителя в базе данных.
+        /// </summary>
+        public async Task EditAsync(Person person)
+        {
+            using (var context = new DataGridContext())
+            {
+                var target = await context.Persons.FirstOrDefaultAsync(x => x.Id == person.Id);
+                if (target != null)
+                {
+                    target.Name = person.Name;
+                    target.Gender = person.Gender;
+                    target.Birhday = person.Birhday;
+                    target.Education = person.Education;
+                    target.Value1 = person.Value1;
+                    target.Value2 = person.Value2;
+                    target.Value3 = person.Value3;
+                }
+                await context.SaveChangesAsync();
+            }
+        }
+
+        /// <summary>
+        /// Получает всех заявителей из базы данных.
+        /// </summary>
+        public async Task<IReadOnlyCollection<Person>> GetAllAsync()
+        {
+            using (var context = new DataGridContext())
+            {
+                var items = await context.Persons
+                    .OrderByDescending(x => x.Name)
+                    .ToListAsync()
+                    ;
+                return items;
+            }
+        }
+    }
+}
---
+++ DtaGridViewShelkynov.StoreMemory.DataBase/DataGridContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+using DataGridViewShelkynov.Contracts;
+using DataGridViewShelkynov.Contracts.Models;
+using DataGridView_Shelkynov.Models;
+
+namespace DtaGridViewShelkynov.StoreMemory.DataBase
+{
+    /// <summary>
+    /// Контекст базы данных
+    /// </summary>
+    public class DataGridContext : DbContext
+    {
+        /// <summary>
+        /// Конструктор контекста базы данных
+        /// </summary>
+        public DataGridContext() : base("DataGridPerson")
+        {
+        }
+
+        /// <summary>
+        /// Таблица <see cref="Person"/> в базе данных
+        /// </summary>
+        public DbSet<Person> Persons { get; set; }
+    }
+}
